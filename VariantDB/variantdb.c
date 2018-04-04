f468af9cbcb79064a7a7c27d722328956ed59f70
Martin Drab <martin.drab@email.cz>
2018-04-04 10:06:36 +0200

    [variantDB]: Initialize command-line options

--- VariantDB/variantdb.c
+++ VariantDB/variantdb.c
@@ -4,6 +4,24 @@
 #include "err.h"
 #include "utils.h"
 #include "options.h"
+#include "variantdb.h"
+
+
+
+static void _cmd_option_init(void)
+{
+	CMD_OPTION_INIT(VDB_OPTION_REF_FILE, String, "");
+	CMD_OPTION_INIT(VDB_OPTION_SAM_FILE, String, "");
+	CMD_OPTION_INIT(VDB_OPTION_VCF_FILE, String, "");
+	CMD_OPTION_INIT(VDB_OPTION_BED_FILE, String, "");
+	CMD_OPTION_INIT(VDB_OPTION_CHROM, String, "1");
+	CMD_OPTION_INIT(VDB_OPTION_START, UInt64, 0);
+	CMD_OPTION_INIT(VDB_OPTION_STOP, UInt64, (uint64_t)-1);
+	CMD_OPTION_INIT(VDB_OPTION_HELP, Boolean, FALSE);
+	CMD_OPTION_INIT(VDB_OPTION_VERBOSE, Boolean, FALSE);
+
+	return;
+}
 
 
 
@@ -16,6 +34,11 @@ int main(int argc, char **argv)
 	if (ret == ERR_SUCCESS) {
 		ret = options_module_init(37);
 		if (ret == ERR_SUCCESS) {
+			_cmd_option_init();
+			ret = options_parse_command_line(argc - 1, argv + 1);
+			if (ret == ERR_SUCCESS) {
+
+			}
 
 			options_module_finit();
 		}
---
+++ VariantDB/variantdb.h
@@ -0,0 +1,45 @@
+
+#ifndef __VARIANT_DB_H__
+#define __VARIANT_DB_H__
+
+
+#define VDB_OPTION_REF_FILE			"ref-file"
+#define VDB_OPTION_SAM_FILE			"sam-file"
+#define VDB_OPTION_VCF_FILE			"vcf-file"
+#define VDB_OPTION_BED_FILE			"bed-file"
+#define VDB_OPTION_CHROM			"chrom"
+#define VDB_OPTION_START			"start"
+#define VDB_OPTION_STOP				"stop"
+#define VDB_OPTION_HELP				"help"
+#define VDB_OPTION_VERBOSE			"verbose"
+
+#define VDB_OPTION_REF_FILE_DESC	"ref-file"
+#define VDB_OPTION_SAM_FILE_DESC	"sam-file"
+#define VDB_OPTION_VCF_FILE_DESC	"vcf-file"
+#define VDB_OPTION_BED_FILE_DESC	"bed-file"
+#define VDB_OPTION_CHROM_DESC		"chrom"
+#define VDB_OPTION_START_DESC		"start"
+#define VDB_OPTION_STOP_DESC		"stop"
+#define VDB_OPTION_HELP_DESC		"help"
+#define VDB_OPTION_VERBOSE_DESC		"verbose"
+
+#define VDB_OPTION_REF_FILE_SHORT	'f'
+#define VDB_OPTION_SAM_FILE_SHORT	's'
+#define VDB_OPTION_VCF_FILE_SHORT	'v'
+#define VDB_OPTION_BED_FILE_SHORT	'b'
+#define VDB_OPTION_CHROM_SHORT		'c'
+#define VDB_OPTION_START_SHORT		'f'
+#define VDB_OPTION_STOP_SHORT		't'
+#define VDB_OPTION_HELP_SHORT		'h'
+#define VDB_OPTION_VERBOSE_SHORT	'V'
+
+
+#define CMD_OPTION_INIT(aMacroName, aType, aDefault)	\
+	option_add_##aType(aMacroName, aDefault);	\
+	option_set_shortcut(aMacroName, aMacroName##_SHORT);	\
+	option_set_description_const(aMacroName, aMacroName##_DESC);	\
+
+#define CMD_OPTION_GET(aMacroName, aType, aPResult)	\
+	option_get_##aType(aMacroName, aResult);	\
+
+#endif
