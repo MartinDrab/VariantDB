f1937b72872252837fdd7aa0d05b6e35edc4b18a
Martin Drab <martin.drab@email.cz>
2018-05-24 13:11:44 +0200

    [variantdb]: Display near variants

--- VariantDB/variantdb.c
+++ VariantDB/variantdb.c
@@ -93,6 +93,7 @@ static boolean _bedLoaded = FALSE;
 KHASH_MAP_INIT_INT64(VariantTableType, PVCF_VARIANT);
 
 khash_t(VariantTableType) *_variantTable = NULL;
+khash_t(VariantTableType) *_nearVariantTable = NULL;
 
 static size_t _readsProcessed = 0;
 
@@ -108,7 +109,7 @@ static ERR_VALUE _on_read_callback(const ONE_READ *Read, void *Context)
 	GEN_ARRAY_char refArray;
 	GEN_ARRAY_char altArray;
 	size_t readSeqIndex = 0;
-	VCF_VARIANT v;
+	PVCF_VARIANT v = NULL;
 	khiter_t it;
 
 	dym_array_init_char(&refArray, 140);
@@ -169,15 +170,49 @@ static ERR_VALUE _on_read_callback(const ONE_READ *Read, void *Context)
 
 						dym_array_push_back_char(&refArray, '\0');
 						dym_array_push_back_char(&altArray, '\0');
-						ret = input_variant_create(Read->Extension->RName, NULL, variantPos, refArray.Data, altArray.Data, 30, &v);
+						ret = utils_malloc(sizeof(VCF_VARIANT), &v);
 						if (ret == ERR_SUCCESS) {
-							input_variant_normalize(refData.Sequence, &v);
-							it = kh_get(VariantTableType, _variantTable, variantPos);
-							if (it != kh_end(_variantTable) &&
-								input_variant_equal(&v, kh_value(_variantTable, it)))
-								kh_value(_variantTable, it)->ReadSupport++;
+							memset(v, 0, sizeof(VCF_VARIANT));
+							ret = input_variant_create(Read->Extension->RName, NULL, variantPos, refArray.Data, altArray.Data, 30, v);
+							if (ret == ERR_SUCCESS) {
+								input_variant_normalize(refData.Sequence, v);
+								it = kh_get(VariantTableType, _variantTable, v->Pos);
+								if (it != kh_end(_variantTable) &&
+									input_variant_equal(v, kh_value(_variantTable, it))) {
+									kh_value(_variantTable, it)->ReadSupport++;
+									input_free_variant(v);
+									utils_free(v);
+									v = NULL;
+								} else {
+									int res = 0;
+									PVCF_VARIANT tmp = NULL;
+									PVCF_VARIANT tmp2 = NULL;
 
-							input_free_variant(&v);
+									it = kh_put(VariantTableType, _nearVariantTable, v->Pos, &res);
+									if (res == 0) {
+										tmp = kh_value(_nearVariantTable, it);
+										tmp2 = tmp;
+										while (tmp2 != NULL) {
+											if (input_variant_equal(tmp2, v)) {
+												++tmp2->ReadSupport;
+												input_free_variant(v);
+												utils_free(v);
+												v = NULL;
+												break;
+											}
+
+											tmp2 = tmp2->Alternative;
+										}
+
+										if (tmp2 == NULL)
+											v->Alternative = tmp;
+									}
+
+									if (v != NULL)
+										kh_value(_nearVariantTable, it) = v;
+
+								}
+							}
 						}
 
 						dym_array_clear_char(&refArray);
@@ -217,6 +252,7 @@ int main(int argc, char **argv)
 	ERR_VALUE ret = ERR_INTERNAL_ERROR;
 
 	_variantTable = kh_init(VariantTableType);
+	_nearVariantTable = kh_init(VariantTableType);
 	ret = utils_allocator_init(1);
 	if (ret == ERR_SUCCESS) {
 		ret = options_module_init(37);
@@ -296,11 +332,19 @@ int main(int argc, char **argv)
 
 				if (ret == ERR_SUCCESS) {
 					PVCF_VARIANT v = variants.Data;
+					PVCF_VARIANT tmp = NULL;
 
 					fprintf(stderr, "\n");
 					fprintf(stderr, "[INFO]: Processing variants...\n");
 					for (size_t i = 0; i < gen_array_size(&variants); ++i) {
 						fprintf(stdout, "%s\t%llu\t%s\t%s\t%s\t%zu\t%zu\n", v->Chrom, v->Pos + 1, v->ID, v->Ref, v->Alt, v->ReadSupport, v->TotalReadsAtPosition);
+						for (size_t j = v->Pos - 10; j < v->Pos + 10; ++j) {
+							khiter_t it = kh_get(VariantTableType, _nearVariantTable, j);
+							if (it != kh_end(_nearVariantTable)) {
+								tmp = kh_val(_nearVariantTable, it);
+								fprintf(stdout, "\t%s\t%llu\t%s\t%s\t%s\t%zu\t%zu\n", tmp->Chrom, tmp->Pos + 1, tmp->ID, tmp->Ref, tmp->Alt, tmp->ReadSupport, tmp->TotalReadsAtPosition);
+							}
+						}
 						++v;
 					}
 				}
